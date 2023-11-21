022909b93cd381bd73ff7603b47804a6131113f8
Maxim <kmaxusk@gmail.com>
2023-11-21 09:06:08 +0200

    fetch function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("object");
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_CYfDoS5dNeACSCwYtMK32XB821HsGAUlrTtg1vSBWMymimcVnHbbjgtqAnOT394U";
+
+const API_URL = 'https://api.thecatapi.com/v1/breeds'
+
+function fetchBreeds(){
+    return fetch(`${API_URL}`)
+    .then((data) => data.json())    
+    .catch(err => console.log(err))    
+}
+
+// .then(arr => arr.forEach(element => {
+// console.log(element.name);
+// }));
+const arr = fetchBreeds()
+.then(data => console.log(data))
+.catch(err => console.log(err));
